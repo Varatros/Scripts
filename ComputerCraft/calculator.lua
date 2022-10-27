8576ad63f6ed3684e170d062917bcbf44d2a942d
Michael Coffman <mjc.coffman@gmail.com>
2022-10-27 16:58:19 -0400

    calculator script done

--- ComputerCraft/calculator.lua
+++ ComputerCraft/calculator.lua
@@ -1,1 +1,11 @@
-print("HELLO WORLD")
+-- The following code was made by Varatros. Please don't reupload this or edit it.
+-- If you have any suggestions, open an issue at my github: https://github.com/Varatros/Scripts
+
+-- Warning, this script is highly vulnerable because it can allow users to run any code on a computer.
+-- If you have a custom os that limits user permissions and such, don't use this script as it bypasses any security.
+
+local input = read()
+local eval, err = loadstring("return " .. input) 
+if err then print("Could not evaluate. " .. err) end
+print("="..eval().."\n")
+shell.run("calculator.lua")
